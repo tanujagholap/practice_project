a80adbd99be150b7e6a108398cd11ffbfce3fe39
demo1 <tanujarbhong@gmail.com>
2024-04-02 15:15:14 +0530

    initial commit

---
+++ pro1/app1/models.py
@@ -0,0 +1,13 @@
+from django.core import validators
+from django.db import models
+
+
+class Create(models.Model):
+    choice2 = [('male', 'MALE'), ('female', 'FEMALE')]
+    choice1 = (('YES', 'yes'), ('NO', 'no'))
+    f_name = models.CharField(max_length=20)
+    s_name = models.CharField(max_length=20)
+    mobile_no = models.CharField(max_length=20, validators=[validators.RegexValidator('[7-9]{1}+[0-9]{9}')])
+    rooms = models.IntegerField()
+    ac = models.CharField(max_length=10, choices=choice1)
+    gender = models.CharField(max_length=10, choices=choice2)
